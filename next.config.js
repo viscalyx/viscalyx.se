0c2b86358abdb73436b8dc5d76a5141dcd3ae8f0
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-05-26 21:03:50 +0200

    Remove swcMinify option from Next.js configuration

--- next.config.js
+++ next.config.js
@@ -21,7 +21,6 @@ const nextConfig = {
   poweredByHeader: false,
   generateEtags: true,
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
