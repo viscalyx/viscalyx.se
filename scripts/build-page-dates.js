c490e920c60a9963725b34d5c2da9c1105281a9a
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-06-01 12:13:13 +0200

    style: update module imports to use node: prefix for consistency

--- scripts/build-page-dates.js
+++ scripts/build-page-dates.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
-const fs = require('fs')
-const path = require('path')
+const { execSync } = require('node:child_process')
+const fs = require('node:fs')
+const path = require('node:path')
 
 /**
  * Get the last modified date of a file from Git history
