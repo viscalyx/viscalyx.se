045fddd52e45b118dd2d5e771d186b93d080539a
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-06-29 05:03:54 +0000

    fix: formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '../lib/theme-context'
-import './globals.css'
 import './code-block-components.css'
+import './globals.css'
 import { metadata } from './metadata'
 import './prism-theme.css'
 
