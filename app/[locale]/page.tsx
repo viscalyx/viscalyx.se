68047262c8ca7594729f2b725da95343e9a35bb2
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-02-14 14:55:30 +0000

    Refactor Home component to remove framer-motion and simplify structure

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,27 +1,19 @@
-'use client'
-
 import About from '@/components/About'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import OpenSource from '@/components/OpenSource'
 import ScrollToTop from '@/components/ScrollToTop'
-import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <motion.main
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="min-h-screen"
-    >
+    <main className="min-h-screen">
       <Header />
       <Hero />
       <About />
       <OpenSource />
       <Footer />
       <ScrollToTop />
-    </motion.main>
+    </main>
   )
 }
